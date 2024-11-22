55a90e308b34429e67637a8e29a5a51cbdfd03d6
antonyevchik <av.yevchik@gmail.com>
2024-11-22 09:36:20 +0200

    tweak the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,30 @@
 @extends('adminlte::auth.auth-page', ['auth_type' => 'login'])
 
 @section('auth_body')
-    <form action="{{ route('login') }}" method="post">
+    <form action="{{ route('login') }}" method="POST">
         @csrf
-
         <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required autofocus>
+            <input type="email" name="email" class="form-control" placeholder="@lang('adminlte::adminlte.email')" value="{{ old('email') }}" required autofocus>
             <div class="input-group-append">
                 <div class="input-group-text">
                     <span class="fas fa-envelope"></span>
                 </div>
             </div>
+            @error('email')
+            <span class="text-red text-sm">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password" required>
+            <input type="password" name="password" class="form-control" placeholder="@lang('adminlte::adminlte.password')" required>
             <div class="input-group-append">
                 <div class="input-group-text">
                     <span class="fas fa-lock"></span>
                 </div>
             </div>
+            @error('password')
+                <span class="text-red text-sm">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="row">
@@ -27,12 +32,12 @@
                 <div class="icheck-primary">
                     <input type="checkbox" name="remember" id="remember">
                     <label for="remember">
-                        Remember Me
+                        @lang('adminlte::adminlte.remember_me')
                     </label>
                 </div>
             </div>
             <div class="col-4">
-                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                <button type="submit" class="btn btn-primary btn-block">@lang('adminlte::adminlte.sign_in')</button>
             </div>
         </div>
     </form>
